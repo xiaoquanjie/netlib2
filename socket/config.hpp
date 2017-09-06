f2a900561a831246925f798bd5821ef107f40bac
xiaoquanjie <xiaoquanjie@ixianlai.com>
2017-09-06 16:44:10 +0800

    macro check

--- socket/config.hpp
+++ socket/config.hpp
@@ -17,12 +17,16 @@
 #define M_SOCKET_NAMESPACE_BEGIN namespace SocketLib{
 #define M_SOCKET_NAMESPACE_END   }
 
+#ifndef M_PLATFORM_WIN32
 #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
 #define M_PLATFORM_WIN32 1
 #endif
+#endif
 
+#ifndef M_PLATFORM_WIN
 #if defined(M_PLATFORM_WIN32) || defined(WIN64)
 #define M_PLATFORM_WIN 1
+#endif
 #endif
 
 #include <assert.h>
