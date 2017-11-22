590a497b111e92b2cfda9ce1adee77ab631fd1df
xiaoquanjie <xiaoquanjie@ixianlai.com>
2017-11-22 11:44:32 +0800

    fix

--- netio/netio.hpp
+++ netio/netio.hpp
@@ -81,16 +81,16 @@ public:
 	 */
 
 	// 连线通知,这个函数里不要处理业务，防止堵塞
-	virtual void OnConnected(TcpSocketPtr& clisock);
-	virtual void OnConnected(TcpConnectorPtr& clisock,SocketLib::SocketError error);
+	virtual void OnConnected(const TcpSocketPtr& clisock);
+	virtual void OnConnected(const TcpConnectorPtr& clisock,SocketLib::SocketError error);
 
 	// 掉线通知,这个函数里不要处理业务，防止堵塞
-	virtual void OnDisconnected(TcpSocketPtr& clisock);
-	virtual void OnDisconnected(TcpConnectorPtr& clisock);
+	virtual void OnDisconnected(const TcpSocketPtr& clisock);
+	virtual void OnDisconnected(const TcpConnectorPtr& clisock);
 
 	// 数据包通知,这个函数里不要处理业务，防止堵塞
-	virtual void OnReceiveData(TcpSocketPtr& clisock, SocketLib::Buffer& buffer);
-	virtual void OnReceiveData(TcpConnectorPtr& clisock, SocketLib::Buffer& buffer);
+	virtual void OnReceiveData(const TcpSocketPtr& clisock, SocketLib::Buffer& buffer);
+	virtual void OnReceiveData(const TcpConnectorPtr& clisock, SocketLib::Buffer& buffer);
 
 protected:
 	void AcceptHandler(SocketLib::SocketError error, TcpSocketPtr& clisock, NetIoTcpAcceptorPtr& acceptor);
--- netio/netio_impl.hpp
+++ netio/netio_impl.hpp
@@ -116,26 +116,26 @@ void NetIo::AcceptHandler(SocketLib::SocketError error, TcpSocketPtr& clisock, N
 */
 
 // 连线通知,这个函数里不要处理业务，防止堵塞
-void NetIo::OnConnected(TcpSocketPtr& clisock) {
+void NetIo::OnConnected(const TcpSocketPtr& clisock) {
 
 }
-void NetIo::OnConnected(TcpConnectorPtr& clisock, SocketLib::SocketError error) {
+void NetIo::OnConnected(const TcpConnectorPtr& clisock, SocketLib::SocketError error) {
 
 }
 
 // 掉线通知,这个函数里不要处理业务，防止堵塞
-void NetIo::OnDisconnected(TcpSocketPtr& clisock) {
+void NetIo::OnDisconnected(const TcpSocketPtr& clisock) {
 
 }
-void NetIo::OnDisconnected(TcpConnectorPtr& clisock) {
+void NetIo::OnDisconnected(const TcpConnectorPtr& clisock) {
 
 }
 
 // 数据包通知,这个函数里不要处理业务，防止堵塞
-void NetIo::OnReceiveData(TcpSocketPtr& clisock, SocketLib::Buffer& buffer) {
+void NetIo::OnReceiveData(const TcpSocketPtr& clisock, SocketLib::Buffer& buffer) {
 
 }
-void NetIo::OnReceiveData(TcpConnectorPtr& clisock, SocketLib::Buffer& buffer) {
+void NetIo::OnReceiveData(const TcpConnectorPtr& clisock, SocketLib::Buffer& buffer) {
 
 }
 
