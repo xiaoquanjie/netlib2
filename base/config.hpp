993311a7a8dbc0076015362b8dd87803b1c60d92
xiaoquanjie <xiaoquanjie@ixianlai.com>
2018-01-03 17:52:06 +0800

    类型定义修改

--- base/config.hpp
+++ base/config.hpp
@@ -16,4 +16,24 @@
 #endif
 #endif
 
+M_BASE_NAMESPACE_BEGIN
+/*
+基本数据类型
+windows: 使用ILP32数据模型 及 LLP64数据模型
+linux:	 平台使用 ILP32 及 LP64 数据模型
+*/
+#ifndef M_PLATFORM_DATA_MODE 
+#define M_PLATFORM_DATA_MODE
+typedef signed char				s_int8_t;
+typedef signed short			s_int16_t;
+typedef signed int				s_int32_t;
+typedef signed long long		s_int64_t;
+typedef unsigned char			s_uint8_t;
+typedef unsigned short			s_uint16_t;
+typedef unsigned int			s_uint32_t;
+typedef unsigned long long		s_uint64_t;
+typedef char					s_byte_t;
+#endif  // M_PLATFORM_DATA_MODE
+M_BASE_NAMESPACE_END
+
 #endif
--- socket/type.hpp
+++ socket/type.hpp
@@ -14,6 +14,7 @@
 #ifndef M_TYPE_INCLUDE
 #define M_TYPE_INCLUDE
 
+#include "base/config.hpp"
 M_SOCKET_NAMESPACE_BEGIN
 
 /*
@@ -21,18 +22,15 @@ M_SOCKET_NAMESPACE_BEGIN
 windows: 使用ILP32数据模型 及 LLP64数据模型
 linux:	 平台使用 ILP32 及 LP64 数据模型
 */
-#ifndef M_PLATFORM_DATA_MODE 
-#define M_PLATFORM_DATA_MODE
-typedef signed char				s_int8_t;
-typedef signed short			s_int16_t;
-typedef signed int				s_int32_t;
-typedef signed long long		s_int64_t;
-typedef unsigned char			s_uint8_t;
-typedef unsigned short			s_uint16_t;
-typedef unsigned int			s_uint32_t;
-typedef unsigned long long		s_uint64_t;
-typedef char					s_byte_t;
-#endif  // M_PLATFORM_DATA_MODE
+typedef base::s_int8_t		s_int8_t;
+typedef base::s_int16_t		s_int16_t;
+typedef base::s_int32_t		s_int32_t;
+typedef base::s_int64_t		s_int64_t;
+typedef base::s_uint8_t		s_uint8_t;
+typedef base::s_uint16_t	s_uint16_t;
+typedef base::s_uint32_t	s_uint32_t;
+typedef base::s_uint64_t	s_uint64_t;
+typedef base::s_byte_t		s_byte_t;
 
 #ifdef M_PLATFORM_WIN
 typedef OVERLAPPED	overlapped_t;
