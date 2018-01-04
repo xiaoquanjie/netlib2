5a02945568bd646c88fcd6f808e3d6b9618ebd8c
xiaoquanjie <xiaoquanjie@ixianlai.com>
2018-01-04 20:25:19 +0800

    en

--- synccall_test.cpp
+++ synccall_test.cpp
@@ -2,6 +2,24 @@
 
 void print_clock(bool beg);
 
+struct CDisplayTime
+{
+	clock_t     miStartTime;
+	const char* mpName;
+	CDisplayTime(const char* pName) :mpName(pName){
+		miStartTime = clock();
+	}
+	~CDisplayTime(){
+		if (mpName){
+			std::cout << mpName << " elapsed=" << ((double)(clock() - miStartTime) / CLOCKS_PER_SEC) << std::endl;
+		}
+	}
+};
+
+
+#define M_DISPLAYTIME() CDisplayTime loDisTime_##__LINE__(__FUNCTION__);
+
+
 class RpcHandler : public synccall::IServerHandler {
 public:
 	virtual void OnOneWayDealer(const int msg_type, netiolib::Buffer& request) {
@@ -24,6 +42,7 @@ public:
 void synccall_server(){
 	synccall::SyncCallSvr server;
 	server.Start(4);
+	//server.RegisterHandler("172.16.81.247", 4001, new RpcHandler);
 	server.RegisterHandler("127.0.0.1", 4001, new RpcHandler);
 	int i = 0;
 	std::cin >> i;
@@ -32,39 +51,42 @@ void synccall_server(){
 
 void synccall_client() {
 	synccall::SyncCallClient client;
-	if (!client.Connect("127.0.0.1", 4001, 2)) {
+	//if (!client.Connect("172.16.81.247", 4001,-1)) {
+	if (!client.Connect("127.0.0.1", 4001, -1)) {
 		std::cout << "connect fail" << std::endl;
 		return;
 	}
+	std::cout << "connect success" << std::endl;
 
 	int i = 0;
 	SocketLib::Buffer request;
 	std::string info = "this is a test!!!!";
 	for (int i = 0; i < 1024; ++i)
 		info.append("a");
-	print_clock(true);
-	while (true) {
-		
-		//std::cin >> info;
-		if (info=="stop")
-			break;
+	
+	{
+		M_DISPLAYTIME();
+		while (true) {
+			//std::cin >> info;
+			if (info == "stop")
+				break;
 
-		request.Clear();
-		request.Write(info);
-		SocketLib::Buffer* reply;
-		int ret = client.SyncCall(1, request.Data(), request.Length(), reply);
-		if (ret != 0) {
-			std::cout << "happend error" << std::endl;
-			break;
+			request.Clear();
+			request.Write(info);
+			SocketLib::Buffer* reply;
+			int ret = client.SyncCall(1, request.Data(), request.Length(), reply);
+			if (ret != 0) {
+				std::cout << "happend error" << std::endl;
+				break;
+			}
+			info.clear();
+			reply->Read(info);
+			info.clear();
+			//std::cout << "server reply:" << info << std::endl;
+			++i;
+			if (i >= 100000)
+				break;
 		}
-		info.clear();
-		reply->Read(info);
-		info.clear();
-		//std::cout << "server reply:" << info << std::endl;
-		++i;
-		if (i>=100000)
-			break;
 	}
-	print_clock(false);
 }
 
