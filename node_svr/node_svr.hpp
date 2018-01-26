279cbab246fce1e74ca1c89cbdf6fbc881156e3b
xiaoquanjie <xiaoquanjie@ixianlai.com>
2018-01-26 21:56:53 +0800

    en

--- node_svr/node_svr.hpp
+++ node_svr/node_svr.hpp
@@ -5,6 +5,94 @@
 #include <netio/netio.hpp>
 M_NODESVR_NAMESPACE_BEGIN
 
+#define message_id_define(msg,id) \
+enum message_id_##id{ \
+	msg = id,\
+};\
+
+message_id_define(msg_id_keepalive, 1);
+message_id_define(msg_id_keepalive_ack, 2);
+message_id_define(msg_id_transfer, 3);
+
+struct protocol_base {
+	base::s_int32_t msgid;
+	protocol_base(base::s_int32_t id)
+		:msgid(id) {
+	}
+	virtual void Read(base::Buffer& buffer) {
+		buffer.Read(msgid);
+	}
+	virtual void Write(base::Buffer& buffer) {
+		buffer.Write(msgid);
+	}
+};
+
+struct protocol_keepalive :
+	public protocol_base
+{
+	protocol_keepalive()
+		:protocol_base(msg_id_keepalive) {
+	}
+	virtual void Read(base::Buffer& buffer) {
+	}
+	virtual void Write(base::Buffer& buffer) {
+		protocol_base::Write(buffer);
+	}
+};
+
+struct protocol_keepalive_ack :
+	public protocol_base
+{
+	protocol_keepalive_ack()
+		:protocol_base(msg_id_keepalive_ack) {
+	}
+	virtual void Read(base::Buffer& buffer) {
+	}
+	virtual void Write(base::Buffer& buffer) {
+		protocol_base::Write(buffer);
+	}
+};
+
+struct protocol_transfer :
+	public protocol_base
+{
+	enum {
+		enum_login_type = 1,
+		enum_logout_type = 2,
+		enum_signle_point_type = 3,
+		enum_type_broadcast_type = 4,
+		enum_all_broadcast_type = 5,
+	};
+	base::s_int32_t msgtype;
+	base::s_int32_t from_token_type;
+	base::s_int32_t from_token_id;
+	base::s_int32_t to_token_type;
+	base::s_int32_t to_token_id;
+	protocol_transfer()
+		:protocol_base(msg_id_transfer) {
+		msgtype = 0;
+		from_token_type = 0;
+		from_token_id = 0;
+		to_token_type = 0;
+		to_token_id = 0;
+	}
+	void Read(base::Buffer& buffer) {
+		buffer.Read(msgtype);
+		buffer.Read(from_token_type);
+		buffer.Read(from_token_id);
+		buffer.Read(to_token_type);
+		buffer.Read(to_token_id);
+	}
+	void Write(base::Buffer& buffer) {
+		protocol_base::Write(buffer);
+		buffer.Write(msgtype);
+		buffer.Write(from_token_type);
+		buffer.Write(from_token_id);
+		buffer.Write(to_token_type);
+		buffer.Write(to_token_id);
+	}
+};
+
 struct protocol_hdr {
 	enum {
 		enum_login_type = 1,
@@ -57,6 +145,9 @@ public:
 
 protected:
 	virtual void OnConnected(netiolib::TcpConnectorPtr& clisock, SocketLib::SocketError error) {
+		if (!error) {
+			_beat.OnConnected(clisock, 10);
+		}
 		_node->OnConnected(clisock, error);
 	}
 
@@ -65,6 +156,8 @@ protected:
 	}
 
 	virtual void OnReceiveData(netiolib::TcpConnectorPtr& clisock, SocketLib::Buffer& buffer) {
+		_beat.OnReceiveData(clisock);
+
 		protocol_hdr message;
 		message.Read(buffer);
 		if (message.msgtype
@@ -94,7 +187,12 @@ protected:
 		}
 	}
 
+	netiolib::HeartBeatMng& GetBeatMng() {
+		return _beat;
+	}
+
 protected:
+	netiolib::HeartBeatMng _beat;
 	ClientNodeType* _node;
 };
 
@@ -109,6 +207,7 @@ public:
 
 	virtual void Start(int thread_cnt) {
 		_netio.Start(thread_cnt);
+		//_netio.get
 	}
 
 	virtual void Stop() {
--- node_svr/transfer_svr.cpp
+++ node_svr/transfer_svr.cpp
@@ -2,10 +2,33 @@
 #include <iostream>
 #include <map>
 #include <fstream>
+#include "base/configreader.hpp"
 
 M_NODESVR_NAMESPACE_BEGIN
 
+class TransferSvr 
+	: public netiolib::NetIo
+{
+public:
+	virtual void OnConnected(netiolib::TcpSocketPtr& clisock) {
 
+	}
+
+	virtual void OnDisconnected(netiolib::TcpSocketPtr& clisock) {
+
+	}
+
+	virtual void OnReceiveData(netiolib::TcpSocketPtr& clisock, SocketLib::Buffer& buffer) {
+
+	}
+
+	netiolib::HeartBeatMng& GetHeartMng() {
+		return _heart_mng;
+	}
+
+protected:
+	netiolib::HeartBeatMng _heart_mng;
+};
 
 
 
